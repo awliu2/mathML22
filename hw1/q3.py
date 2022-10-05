d79415cf2978ddcf1b04bc3d88b1206b7d6c03e0
Andi Willow Liu <awliu2@uchicago.edu>
2022-10-05 16:14:51 -0500

    finished hw1

---
+++ hw1/q3.py
@@ -0,0 +1,30 @@
+from re import L
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+# n = number of points
+# z = points where polynomial is evaluated
+# p = array to store the values of the interpolated polynomials
+n = 100
+z = np.linspace(-1, 1, n)
+p = []
+
+d = 3 # degree
+w = np.random.rand(d) 
+X = np.zeros((n,d))
+
+
+# generate X-matrix
+for i in range(n):
+    for j in range(3):
+        X[i][j] = z[i] ** j
+
+# evaluate polynomial at all points z, and store the result in p # do NOT use a loop for this
+p = np.matmul(X, w)
+
+# plot the datapoints and the best-fit polynomials
+plt.plot(z, p, linewidth=2)
+plt.xlabel('z')
+plt.ylabel('y')
+plt.title('polynomial_with_coefficients_w_=_%s' %w )
+plt.show()
